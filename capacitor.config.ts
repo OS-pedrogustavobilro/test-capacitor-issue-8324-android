6520da6b26596cecbdcec2efdbf2f574d190cd60
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-02-03 09:19:33 +0000

    chore: ionic start

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'test-capacitor-issue-8324-android',
+  webDir: 'dist'
+};
+
+export default config;
